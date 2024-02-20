62a86106460dbbfbdf57d586df85313a8df88ec2
Pedro Dias <PedroRibeiroDias@outlook.pt>
2024-02-20 14:45:11 +0000

    Add hobbies

---
+++ scripts/data.js
@@ -0,0 +1,157 @@
+const skills = [
+    {name: "JavaScript", img: "js"},
+    {name: "TypeScript", img: "ts"},
+    {name: "Go", img: "go"},
+    {name: "Python", img: "py"},
+    {name: "Node.js", img: "nodejs"},
+    {name: "Express", img: "express"},
+    {name: "Nest.js", img: "nestjs"},
+    {name: "React", img: "react"},
+    {name: "Redux", img: "redux"},
+    {name: "Next.js", img: "nextjs"},
+    {name: "Vue.js", img: "vue"},
+    {name: "Nuxt.js", img: "nuxtjs"},
+    {name: "Angular", img: "angular"},
+    {name: "Jest", img: "jest"},
+    {name: "Vite", img: "vite"},
+    {name: "Django", img: "django"},
+    {name: "Flask", img: "flask"},
+    {name: "FastAPI", img: "fastapi"},
+    {name: "C#", img: "cs"},
+    {name: ".NET", img: "dotnet"},
+    {name: "C", img: "c"},
+    {name: "C++", img: "cpp"},
+    {name: "Java", img: "java"},
+    {name: "Spring", img: "spring"},
+    {name: "Kotlin", img: "kotlin"},
+    {name: "Android Studio", img: "androidstudio"},
+    {name: "Rust", img: "rust"},
+    {name: "Solidity", img: "solidity"},
+    {name: "MongoDB", img: "mongodb"},
+    {name: "PostgreSQL", img: "postgres"},
+    {name: "MySQL", img: "mysql"},
+    {name: "Redis", img: "redis"},
+    {name: "SQLite", img: "sqlite"},
+    {name: "HTML", img: "html"},
+    {name: "CSS", img: "css"},
+    {name: "Sass", img: "sass"},
+    {name: "Bootstrap", img: "bootstrap"},
+    {name: "ReactiveX", img: "reactivex"},
+    {name: "Postman", img: "postman"},
+    {name: "Docker", img: "docker"},
+    {name: "Kubernetes", img: "kubernetes"},
+    {name: "Linux", img: "linux"},
+    {name: "Bash", img: "bash"},
+    {name: "Git", img: "git"},
+    {name: "GitHub Actions", img: "githubactions"},
+];
+
+const education_en = [
+    {
+        title: "Google Cibersecurity Professional Certificate",
+        institution: "Coursera",
+        date: "2023",
+        badge: "google-cybersecurity-certificate",
+    },
+    {
+        title: "Google IT Automation with Python Professional Certificate",
+        institution: "Coursera",
+        date: "2023",
+        badge: "google-it-automation-certificate",
+
+    },
+    {
+        title: "Google Project Management Professional Certificate",
+        institution: "Coursera",
+        date: "2023",
+        badge: "google-project-management-certificate.2",
+    },
+    {
+        title: "Meta Fulll Satck Engineer",
+        institution: "Coursera",
+        date: "2023",
+        badge: "meta-full-stack-engineer-certificate",
+    },
+    {
+        title: "Meta Generalist Software Engineer",
+        institution: "Coursera",
+        date: "2023",
+        badge: "meta-generalist-software-engineer-certificate",
+    },
+    {
+        title: "Google Data Analytics Professional Certificate",
+        institution: "Coursera",
+        date: "2021",
+        badge: "google-data-analytics-certificate.2",
+    },
+];
+
+const interests_en = [
+    {
+        title: "Open Source",
+        description: "I am passionate about open source software and I love to contribute to open source projects.",
+    },
+    {
+        title: "Cybersecurity",
+        description: "I am passionate about cybersecurity and I am always looking for ways to improve my cybersecurity skills.",
+    },
+    {
+        title: "Artificial Intelligence",
+        description: "I am passionate about artificial intelligence and I am always looking for ways to improve my artificial intelligence skills.",
+    },
+    {
+        title: "Blockchain",
+        description: "I am passionate about blockchain and I am always looking for ways to improve my blockchain skills.",
+    },
+    {
+        title: "Cloud Computing",
+        description: "I am passionate about cloud computing and I am always looking for ways to improve my cloud computing skills.",
+    },
+    {
+        title: "DevOps",
+        description: "I am passionate about DevOps and I am always looking for ways to improve my DevOps skills.",
+    },
+    {
+        title: "Machine Learning",
+        description: "I am passionate about machine learning and I am always looking for ways to improve my machine learning skills.",
+    },
+    {
+        title: "Web Development",
+        description: "I am passionate about web development and I am always looking for ways to improve my web development skills.",
+    },
+];
+
+const hobbies_en = [
+    {
+
+        title: "Listening to Audiobooks",
+        description: "I love to listen to audiobooks and I am always looking for new audiobooks to listen to.",
+        icon: "audiobooks",
+    },
+    {
+        title: "Writing",
+        description: "I love to write and I am always looking for new things to write about.",
+        icon: "writing",
+    },
+    {
+        title: "Music",
+        description: "I love to listen to music and I am always looking for new music to listen to.",
+        icon: "music",
+    },
+    {
+        title: "Movies",
+        description: "I love to watch movies and I am always looking for new movies to watch.",
+        icon: "movies",
+    },
+    {
+        title: "Traveling",
+        description: "I love to travel and I am always looking for new places to visit.",
+        icon: "travel",
+    },
+    {
+        title: "Cooking",
+        description: "I love to cook and I am always looking for new recipes to try.",
+        icon: "cooking",
+    },
+];
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,73 +1,3 @@
-const skills = [
-    {name: "JavaScript", img: "js"},
-    {name: "TypeScript", img: "ts"},
-    {name: "Node.js", img: "nodejs"},
-    {name: "Express", img: "express"},
-    {name: "Nest.js", img: "nestjs"},
-    {name: "React", img: "react"},
-    {name: "Redux", img: "redux"},
-    {name: "Next.js", img: "nextjs"},
-    {name: "Vue.js", img: "vue"},
-    {name: "Nuxt.js", img: "nuxtjs"},
-    {name: "Angular", img: "angular"},
-    {name: "Jest", img: "jest"},
-    {name: "Vite", img: "vite"},
-    {name: "Go", img: "go"},
-    {name: "Python", img: "py"},
-    {name: "Django", img: "django"},
-    {name: "Flask", img: "flask"},
-    {name: "FastAPI", img: "fastapi"},
-    {name: "C#", img: "cs"},
-    {name: ".NET", img: "dotnet"},
-    {name: "C", img: "c"},
-    {name: "C++", img: "cpp"},
-    {name: "Java", img: "java"},
-    {name: "Spring", img: "spring"},
-    {name: "Kotlin", img: "kotlin"},
-    {name: "Android Studio", img: "androidstudio"},
-    {name: "Rust", img: "rust"},
-    {name: "Solidity", img: "solidity"},
-    {name: "MongoDB", img: "mongodb"},
-    {name: "PostgreSQL", img: "postgres"},
-    {name: "MySQL", img: "mysql"},
-    {name: "Redis", img: "redis"},
-    {name: "SQLite", img: "sqlite"},
-    {name: "HTML", img: "html"},
-    {name: "CSS", img: "css"},
-    {name: "Sass", img: "sass"},
-    {name: "Bootstrap", img: "bootstrap"},
-    {name: "ReactiveX", img: "reactivex"},
-    {name: "Postman", img: "postman"},
-    {name: "Docker", img: "docker"},
-    {name: "Kubernetes", img: "kubernetes"},
-    {name: "Linux", img: "linux"},
-    {name: "Bash", img: "bash"},
-    {name: "Git", img: "git"},
-    {name: "GitHub Actions", img: "githubactions"},
-];
-
-const education_en = [
-    {
-        title: "Google Cibersecurity Professional Certificate",
-        institution: "Coursera",
-        date: "2023",
-        badge: "google-cybersecurity-certificate",
-    },
-    {
-        title: "Google IT Automation with Python Professional Certificate",
-        institution: "Coursera",
-        date: "2023",
-        badge: "google-it-automation-certificate",
-
-    },
-    {
-        title: "Google Data Analytics Professional Certificate",
-        institution: "Coursera",
-        date: "2021",
-        badge: "google-data-analytics-certificate.2",
-    },
-]
-
 const app = Vue.createApp({
     data() {
         return {
@@ -79,9 +9,6 @@ const app = Vue.createApp({
 
 app.component('vheader', {
     template: '#vue_header',
-    data() {
-        return undefined;
-    }
 });
 
 app.component('vmain', {
@@ -116,7 +43,25 @@ app.component('skill-list', {
             return skills.filter(skill => skill.name.toLowerCase().includes(skill_search.value.toLowerCase()))
         }
     }
-})
+});
+
+app.component('interests_list', {
+    template: '#vue_interests_list',
+    data() {
+        return {
+            interests: interests_en
+        }
+    }
+});
+
+app.component('hobbies_list', {
+    template: '#vue_hobbies_list',
+    data() {
+        return {
+            hobbies: hobbies_en
+        }
+    }
+});
 
 
 app.mount('#app')
