a2073375242cfe0c60c434af3daaa2c7c83ac89d
Pedro Dias <PedroRibeiroDias@outlook.pt>
2024-02-21 23:38:15 +0000

    Add sctructure and animations

--- scripts/data.js
+++ scripts/data.js
@@ -46,112 +46,254 @@ const skills = [
     {name: "GitHub Actions", img: "githubactions"},
 ];
 
-const education_en = [
-    {
-        title: "Google Cibersecurity Professional Certificate",
-        institution: "Coursera",
-        date: "2023",
-        badge: "google-cybersecurity-certificate",
-    },
-    {
-        title: "Google IT Automation with Python Professional Certificate",
-        institution: "Coursera",
-        date: "2023",
-        badge: "google-it-automation-certificate",
+const skills_en =  {
+    title : "Skills",
+    skills: skills,
+};
 
-    },
-    {
-        title: "Google Project Management Professional Certificate",
-        institution: "Coursera",
-        date: "2023",
-        badge: "google-project-management-certificate.2",
-    },
-    {
-        title: "Meta Fulll Satck Engineer",
-        institution: "Coursera",
-        date: "2023",
-        badge: "meta-full-stack-engineer-certificate",
-    },
-    {
-        title: "Meta Generalist Software Engineer",
-        institution: "Coursera",
-        date: "2023",
-        badge: "meta-generalist-software-engineer-certificate",
-    },
-    {
-        title: "Google Data Analytics Professional Certificate",
-        institution: "Coursera",
-        date: "2021",
-        badge: "google-data-analytics-certificate.2",
-    },
-];
+const banner_en = {
+    title: "Hi 👋, I'm Pedro Dias",
+    catch_phrases: [
+        "Full Stack Developer",
+        "Cybersecurity Enthusiast",
+        "AI and Blockchain Fan",
+        "Fluent in JavaScript, Python, and Go",
+    ],
+    img: "pedribei.jpg",
+    alt: "Profile Picture of Pedro Dias",
+};
 
-const interests_en = [
-    {
-        title: "Open Source",
-        description: "I am passionate about open source software and I love to contribute to open source projects.",
-    },
-    {
-        title: "Cybersecurity",
-        description: "I am passionate about cybersecurity and I am always looking for ways to improve my cybersecurity skills.",
-    },
-    {
-        title: "Artificial Intelligence",
-        description: "I am passionate about artificial intelligence and I am always looking for ways to improve my artificial intelligence skills.",
-    },
-    {
-        title: "Blockchain",
-        description: "I am passionate about blockchain and I am always looking for ways to improve my blockchain skills.",
-    },
-    {
-        title: "Cloud Computing",
-        description: "I am passionate about cloud computing and I am always looking for ways to improve my cloud computing skills.",
-    },
-    {
-        title: "DevOps",
-        description: "I am passionate about DevOps and I am always looking for ways to improve my DevOps skills.",
-    },
-    {
-        title: "Machine Learning",
-        description: "I am passionate about machine learning and I am always looking for ways to improve my machine learning skills.",
-    },
-    {
-        title: "Web Development",
-        description: "I am passionate about web development and I am always looking for ways to improve my web development skills.",
-    },
-];
+const about_en = {
+    title: "About Me",
+    description: ["I am a software developer from Portugal. With a strong background in programming and a passion for continuous learning, " +
+    "I have acquired expertise in various languages including Python, TypeScript, and Golang. My curiosity and drive for self-improvement " +
+    "have led me to constantly seek out new challenges and expand my skillset.",
+        "Last year, I completed several specializations from Google, Meta, and some American Universities, further enhancing my knowledge " +
+        "and proficiency in areas such as Software Development, Cybersecurity and Project Management.",
+        "In addition to my technical skills, I had the privilege of working on multinational projects, contributing to applications used " +
+        "by users worldwide. These experiences have provided me with valuable insights into global software development practices and have " +
+        "sharpened my ability to collaborate effectively across diverse teams and cultures."
+    ],
+}
 
-const hobbies_en = [
-    {
+const certification_en = {
+    title: "Certifications",
+    certifications: [
+        {
+            title: "Google Cibersecurity Professional Certificate",
+            institution: "Coursera",
+            date: "2023",
+            badge: "google-cybersecurity-certificate",
+        },
+        {
+            title: "Google IT Automation with Python Professional Certificate",
+            institution: "Coursera",
+            date: "2023",
+            badge: "google-it-automation-certificate",
 
-        title: "Listening to Audiobooks",
-        description: "I love to listen to audiobooks and I am always looking for new audiobooks to listen to.",
-        icon: "audiobooks",
-    },
-    {
-        title: "Writing",
-        description: "I love to write and I am always looking for new things to write about.",
-        icon: "writing",
-    },
-    {
-        title: "Music",
-        description: "I love to listen to music and I am always looking for new music to listen to.",
-        icon: "music",
-    },
-    {
-        title: "Movies",
-        description: "I love to watch movies and I am always looking for new movies to watch.",
-        icon: "movies",
-    },
-    {
-        title: "Traveling",
-        description: "I love to travel and I am always looking for new places to visit.",
-        icon: "travel",
-    },
-    {
-        title: "Cooking",
-        description: "I love to cook and I am always looking for new recipes to try.",
-        icon: "cooking",
-    },
-];
+        },
+        {
+            title: "Google Project Management Professional Certificate",
+            institution: "Coursera",
+            date: "2023",
+            badge: "google-project-management-certificate.2",
+        },
+        {
+            title: "Meta Fulll Satck Engineer",
+            institution: "Coursera",
+            date: "2023",
+            badge: "meta-full-stack-engineer-certificate",
+        },
+        {
+            title: "Meta Generalist Software Engineer",
+            institution: "Coursera",
+            date: "2023",
+            badge: "meta-generalist-software-engineer-certificate",
+        },
+        {
+            title: "Google Data Analytics Professional Certificate",
+            institution: "Coursera",
+            date: "2021",
+            badge: "google-data-analytics-certificate.2",
+        },
+    ],
+};
 
+
+const languages_en = {
+    title: "Languages",
+    languages: [
+        {
+            name: "Portuguese",
+            level: "Native",
+            percentage: 100,
+        },
+        {
+            name: "English",
+            level: "Full Professional Proficiency",
+            percentage: 100,
+        },
+        {
+            name: "Spanish",
+            level: "Professional Proficiency",
+            percentage: 70,
+        },
+        {
+            name: "French",
+            level: "Elementary Proficiency",
+            percentage: 40,
+        },
+        {
+            name: "German",
+            level: "Elementary Proficiency",
+            percentage: 40,
+        },
+    ],
+
+}
+
+const interests_en = {
+    title: "Interests",
+    interests: [
+        {
+            title: "Open Source",
+            description: "I am passionate about open source software and I love to contribute to open source projects.",
+        },
+        {
+            title: "Cybersecurity",
+            description: "I am passionate about cybersecurity and I am always looking for ways to improve my cybersecurity skills.",
+        },
+        {
+            title: "Artificial Intelligence",
+            description: "I am passionate about artificial intelligence and I am always looking for ways to improve my artificial intelligence skills.",
+        },
+        {
+            title: "Blockchain",
+            description: "I am passionate about blockchain and I am always looking for ways to improve my blockchain skills.",
+        },
+        {
+            title: "Cloud Computing",
+            description: "I am passionate about cloud computing and I am always looking for ways to improve my cloud computing skills.",
+        },
+        {
+            title: "DevOps",
+            description: "I am passionate about DevOps and I am always looking for ways to improve my DevOps skills.",
+        },
+        {
+            title: "Machine Learning",
+            description: "I am passionate about machine learning and I am always looking for ways to improve my machine learning skills.",
+        },
+        {
+            title: "Web Development",
+            description: "I am passionate about web development and I am always looking for ways to improve my web development skills.",
+        },
+    ]
+};
+
+const hobbies_en = {
+    title: "Hobbies",
+    hobbies: [
+        {
+
+            title: "Listening to Audiobooks",
+            description: "I love to listen to audiobooks and I am always looking for new audiobooks to listen to.",
+            icon: "audiobooks",
+        },
+        {
+            title: "Writing",
+            description: "I love to write and I am always looking for new things to write about.",
+            icon: "writing",
+        },
+        {
+            title: "Music",
+            description: "I love to listen to music and I am always looking for new music to listen to.",
+            icon: "music",
+        },
+        {
+            title: "Traveling",
+            description: "I love to travel and I am always looking for new places to visit.",
+            icon: "travel",
+        },
+        {
+            title: "Cooking",
+            description: "I love to cook and I am always looking for new recipes to try.",
+            icon: "cooking",
+        },
+    ]
+};
+
+
+
+const portfolio_en = {
+    title: "Portfolio",
+    projects: [
+        {
+            title: "Ticketing App",
+            description: "An app for selling and buying tickets for events. Made with React, Next.js, Golang, MongoDB, PostgreSQL, and Redis. Using microservices architecture, Docker, and Kubernetes.",
+            img: "ticketing-app",
+        },
+        {
+            title: "E-commerce App",
+            description: "An app for selling and buying products. Made with FastAPI, React, MongoDB and Bootstrap.",
+            img: "e-commerce-app",
+        },
+        {
+            title: "Social Media App",
+            description: "An app for sharing posts and photos. Made with Django, React, PostgreSQL, and Bootstrap.",
+            img: "social-media-app",
+        },
+        {
+            title: "Blog App",
+            description: "An app for writing and sharing blog posts. Made with Flask, React, SQLite, and Sass.",
+            img: "blog-app",
+        },
+        {
+            title: "Chat App",
+            description: "An app for sending and receiving messages. Made with Nest.js, React, MySQL, and Sass.",
+            img: "chat-app",
+        },
+        {
+            title: "E-learning App",
+            description: "An app for publishing and selling online courses. Made with Node.js, Vue.js and MongoDB.",
+            img: "e-learning-app",
+        },
+    ],
+}
+
+const contact_en = {
+    title: "Contact info",
+    socials: [
+        {
+            name: "GitHub",
+            link: "",
+            icon: "github",
+            message: "Check out my GitHub profile",
+        },
+        {
+            name: "LinkedIn",
+            link: "",
+            icon: "linkedin",
+            message: "Check out my LinkedIn profile",
+        },
+    ],
+    formFields: [
+        {
+            name: "name",
+            type: "text",
+            placeholder: "Name",
+            label: "What's your name?",
+        },
+        {
+            name: "email",
+            type: "email",
+            placeholder: "Email",
+            label: "What's your email?",
+        },
+        {
+            name: "message",
+            type: "text",
+            placeholder: "Message",
+            label: "What's your message?",
+        }
+    ],
+}
--- scripts/index.js
+++ scripts/index.js
@@ -6,50 +6,157 @@ const app = Vue.createApp({
     }
 });
 
-
-app.component('vheader', {
-    template: '#vue_header',
+// Header Components
+app.component('vnavbar', {
+    template: '#vue_navbar',
 });
 
-app.component('vmain', {
-    template: '#vue_main'
+// Main Components
+app.component('vbanner', {
+    template: '#vue_banner',
+    setup() {
+        const index = Vue.ref(0);
+        const lineLength = Vue.ref(0);
+        const settings  = Vue.ref({
+            isWritting: true,
+            isPaused: false
+        });
+        const isPaused = Vue.ref(false);
+        return {
+            index,
+            lineLength,
+            settings,
+            isPaused
+        }
+    },
+    created() {
+        let timeRef;
+        let timeRef2;
+        timeRef = setInterval(() => {
+            if (!this.isPaused){
+                if (this.line.length <= this.catch_phrases[this.index].length && this.isWritting) {
+                    this.addLetter();
+                } else if (this.line.length === this.catch_phrases[this.index].length && this.isWritting) {
+                    this.pauseAnimation();
+                    timeRef2 = setTimeout(() => {
+                        this.resumeAnimantion();
+                    }, 1500);
+                } else if (this.line.length > 0 && !this.isWritting) {
+                    this.removeLetter();
+                }
+            // else if (this.line.length >=0 && !this.isWritting) {
+            //     this.lineLength--;
+            // } else if (this.line.length === 0 && !this.isWritting) {
+            //     this.write = true;
+            //     this.isPaused = true;
+            //     setTimeout(() => {
+            //         this.index = (this.index + 1) % this.catch_phrases.length;
+            //         this.isPaused = false;
+            //     }, 1500);
+            // } else this.write = false;
+            }
+            console.log(this.lineLength, this.isWritting, this.isPaused);
+        }, 70);
+        return () => clearInterval(timeRef);
+    },
+    data() {
+        return {
+            ...banner_en
+        }
+    },
+    computed: {
+        line: {
+            get() {
+                return this.catch_phrases[this.index].slice(0, this.lineLength) + '|';
+            }
+        }
+    },
+    methods: {
+        nextPhrase() {
+            this.index = (this.index + 1) % this.catch_phrases.length;
+        },
+        pauseAnimation() {
+            this.settings.isPaused = true;
+            this.settings.isWritting = false;
+        },
+        resumeAnimantion() {
+            this.settings.isPaused = false;
+        },
+        startWritting() {
+            this.settings.isWritting = true;
+        },
+        addLetter() {
+            this.lineLength++;
+        },
+        removeLetter() {
+            this.lineLength--;
+        }
+
+    }
 });
 
+app.component('vabout', {
+    template: '#vue_about',
+    data() {
+        return {
+            ...about_en
+        }
+    }
+});
+
+// About Components
 app.component('vfooter', {
     template: '#vue_footer'
 });
 
-app.component('education_list', {
-    template: '#vue_education_list',
+app.component('certification_list', {
+    template: '#vue_certification_list',
     data() {
         return {
-            education: education_en
+            ...certification_en
         }
     }
 });
 
 
-const skill_search = Vue.ref('');
 app.component('skill-list', {
     template: '#vue_skill_list',
+    setup() {
+        const skill_search = Vue.ref('');
+        return {
+            skill_search
+        }
+    },
     data() {
         return {
-            skills: skills,
-            skill_search: skill_search
+            ...skills_en,
         }
     },
     computed: {
-        filteredSkills() {
-            return skills.filter(skill => skill.name.toLowerCase().includes(skill_search.value.toLowerCase()))
+        filteredSkills: {
+            get() {
+                return skills.filter(skill => skill.name.toLowerCase().includes(this.skill_search.toLowerCase())).slice(0, 6);
+            }
         }
+
     }
 });
+
+app.component('language_list',{
+    template: '#vue_language_list',
+    data() {
+        return {
+            ...languages_en,
+        }
+    }
+
+})
 
 app.component('interests_list', {
     template: '#vue_interests_list',
     data() {
         return {
-            interests: interests_en
+            ...interests_en,
         }
     }
 });
@@ -58,7 +165,26 @@ app.component('hobbies_list', {
     template: '#vue_hobbies_list',
     data() {
         return {
-            hobbies: hobbies_en
+            ...hobbies_en,
+        }
+    }
+});
+// .About Components
+
+app.component('vportfolio', {
+    template: '#vue_portfolio',
+    data() {
+        return {
+            ...portfolio_en
+        }
+    }
+});
+
+app.component('vcontact', {
+    template: '#vue_contact',
+    data() {
+        return {
+            ...contact_en
         }
     }
 });
