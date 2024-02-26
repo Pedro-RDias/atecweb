ac00cb5f26216e30cd446ad8bca1688275a5218e
Pedro Dias <146863707+Pedro-RDias@users.noreply.github.com>
2024-02-26 08:40:04 +0000

    Add Timeline (#2)

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,66 @@
-const language = Vue.reactive(
-    {
-        current: 'en',
-        available: ['en', 'pt'],
-        changeLanguage(lang) {
-            console.log(lang);
-            if (this.available.includes(lang) && lang !== this.current) {
-                this.current = lang;
-            }
+// language Vue state management
+const language = Vue.reactive({
+    current: 'en',
+    available: ['en', 'pt'],
+    changeLanguage(lang) {
+        if (this.available.includes(lang) && lang !== this.current) {
+            this.current = lang;
         }
-    }
-)
+    },
+});
 
+// Language Data Filter
+const getLanguage = (lang) => {
+    switch (lang) {
+        case 'pt':
+            return {
+                navbar: navbar_pt_pt,
+                banner: banner_pt_pt,
+                about: about_pt_pt,
+                skills: skills_pt_pt,
+                languages: languages_pt_pt,
+                interests: interests_pt_pt,
+                hobbies: hobbies_pt_pt,
+                portfolio: portfolio_pt_pt,
+                contact: contact_pt_pt,
+                timeline_banner: resume_banner_pt_pt,
+                timeline: timeline_pt_pt,
+                timelineLabels: timelineLabels_pt_pt,
+            };
+        case 'en':
+        default:
+            return {
+                navbar: navbar_en,
+                banner: banner_en,
+                about: about_en,
+                skills: skills_en,
+                languages: languages_en,
+                interests: interests_en,
+                hobbies: hobbies_en,
+                portfolio: portfolio_en,
+                contact: contact_en,
+                timeline_banner: resume_banner_en,
+                timeline: timeline_en,
+                timelineLabels: timelineLabels_en,
+            };
+    }
+};
+
+// Time Range Formatter
+const getTimeRange = (start, end) => {
+    if (!start || !end) return '';
+    if (
+        Object.prototype.toString.call(start) !== '[object Date]' ||
+        Object.prototype.toString.call(end) !== '[object Date]'
+    )
+        return '';
+    const startDate = start.getMonth() + 1 + '/' + start.getFullYear();
+    const endDate =
+        end.getFullYear() === new Date().getFullYear() ? 'Present' : end.getMonth() + 1 + '/' + end.getFullYear();
+    return `${startDate} - ${endDate}`;
+};
+
+// Vue App
 const app = Vue.createApp();
 
 // Header Components
@@ -22,12 +72,12 @@ app.component('vnavbar', {
             show: false,
             toggleShow() {
                 this.show = window.scrollY >= window.innerHeight - navOffset;
-            }
-        })
+            },
+        });
         return {
             showBackround,
-            language
-        }
+            language,
+        };
     },
     created() {
         window.addEventListener('scroll', this.handleScroll);
@@ -39,34 +89,25 @@ app.component('vnavbar', {
         handleScroll() {
             // Any code to be executed when the window is scrolled
             if (
-                window.scrollY > window.innerHeight - navOffset && !this.showBackround.show
-                || window.scrollY < window.innerHeight - navOffset && this.showBackround.show
-            ) this.showBackround.toggleShow();
-        }
+                (window.scrollY > window.innerHeight - navOffset && !this.showBackround.show) ||
+                (window.scrollY < window.innerHeight - navOffset && this.showBackround.show)
+            )
+                this.showBackround.toggleShow();
+        },
     },
     computed: {
         backgroundStyle: {
             get() {
                 return this.showBackround.show ? 'border-bottom border-body bg-body-tertiary' : '';
-            }
+            },
         },
-        themeStyle: {
-            get() {
-                return this.showBackround.show ? '' : 'dark';
-            }
+        themeStyle() {
+            return this.showBackround.show ? '' : 'dark';
         },
-        info: {
-            get() {
-                switch (language.current) {
-                    case 'pt':
-                        return navbar_pt_pt;
-                    case 'en':
-                    default:
-                        return navbar_en;
-                }
-            }
-        }
-    }
+        info() {
+            return getLanguage(language.current).navbar;
+        },
+    },
 });
 
 // Main Components
@@ -95,10 +136,10 @@ app.component('vbanner', {
                 this.isWritting = false;
                 this.isPaused = true;
             },
-        })
+        });
         return {
-            phraseStore
-        }
+            phraseStore,
+        };
     },
     created() {
         let timeRef;
@@ -120,71 +161,46 @@ app.component('vbanner', {
                     this.phraseStore.startWritting(this.info.jobTitles.length);
                 }
             }
-
-
         }, 50);
         return () => clearInterval(timeRef);
     },
     computed: {
-        line: {
-            get() {
-                return this.info.jobTitles[this.phraseStore.index].slice(0, this.phraseStore.lineLength);
-            },
+        line() {
+            return this.info.jobTitles[this.phraseStore.index].slice(0, this.phraseStore.lineLength);
         },
-        info: {
-            get() {
-                switch (language.current) {
-                    case 'pt':
-                        return banner_pt_pt;
-                    case 'en':
-                    default:
-                        return banner_en;
-                }
-            }
-        }
+        info() {
+            return getLanguage(language.current).banner;
+        },
     },
 });
 
+// About Main Component
 app.component('vabout', {
     template: '#vue_about',
     computed: {
-        info: {
-            get() {
-                switch (language.current) {
-                    case 'pt':
-                        return about_pt_pt;
-                    case 'en':
-                    default:
-                        return about_en;
-                }
-
-            }
-        }
-    }
+        info() {
+            return getLanguage(language.current).about;
+        },
+    },
 });
 
 // About Components
-app.component('vfooter', {
-    template: '#vue_footer'
-});
-
+// Certifications
 app.component('certification_list', {
     template: '#vue_certification_list',
     data() {
         return {
-            ...certification_en
-        }
+            ...certification_en,
+        };
     },
     computed: {
-        certTotal: {
-            get() {
-                return this.certifications.length;
-            }
-        }
-    }
+        certTotal() {
+            return this.certifications.length;
+        },
+    },
 });
 
-// Components
+// Skills
 app.component('skill-list', {
     template: '#vue_skill_list',
     setup() {
@@ -192,141 +208,165 @@ app.component('skill-list', {
         return {
             skill_search,
             language,
-        }
+        };
     },
     computed: {
-        filteredSkills: {
-            get() {
-                const s = skills.filter(skill => {
-                    if (this.skill_search === '') return true;
-                    else if (skill.name.toLowerCase().includes(this.skill_search.toLowerCase())) return true;
-                    else return skill.tags.some(keyword => keyword.toLowerCase().includes(this.skill_search.toLowerCase()));
-                });
+        filteredSkills() {
+            const s = skills.filter((skill) => {
+                if (this.skill_search === '') return true;
+                else if (skill.name.toLowerCase().includes(this.skill_search.toLowerCase())) return true;
+                else return skill.tags.some((keyword) => keyword.toLowerCase().includes(this.skill_search.toLowerCase()));
+            });
 
-                return s.length > 0 ? s.slice(0, 12) : this.skills.slice(0, 12);
-            }
+            return s.length > 0 ? s.slice(0, 12) : this.skills.slice(0, 12);
         },
-        info: {
-            get() {
-                switch (language.current) {
-                    case 'pt':
-                        return skills_pt_pt;
-                    case 'en':
-                    default:
-                        return skills_en;
-                }
-
-            }
+        info() {
+            return getLanguage(language.current).skills;
         },
-
-    }
+    },
 });
 
+// Languages
 app.component('language_list', {
     template: '#vue_language_list',
     computed: {
-        info: {
-            get() {
-                switch (language.current) {
-                    case 'pt':
-                        return languages_pt_pt;
-                    case 'en':
-                    default:
-                        return languages_en;
-                }
-            },
-        }
-    },
-
-});
-
-app.component('interests_list', {
-    template: '#vue_interests_list',
-    computed: {
-        info: {
-            get() {
-                switch (language.current) {
-                    case 'pt':
-                        return interests_pt_pt;
-                    case 'en':
-                    default:
-                        return interests_en;
-                }
-            },
+        info() {
+            return getLanguage(language.current).languages;
         },
     },
 });
 
+// Interests
+app.component('interests_list', {
+    template: '#vue_interests_list',
+    computed: {
+        info() {
+            return getLanguage(language.current).interests;
+        },
+    },
+});
+
+// Hobbies
 app.component('hobbies_list', {
     template: '#vue_hobbies_list',
     computed: {
-        info: {
-            get() {
-                switch (language.current) {
-                    case 'pt':
-                        return hobbies_pt_pt;
-                    case 'en':
-                    default:
-                        return hobbies_en;
-                }
-            },
+        info() {
+            return getLanguage(language.current).hobbies;
         },
     },
 });
 // .About Components
 
+// Portfolio Component
 app.component('vportfolio', {
     template: '#vue_portfolio',
     computed: {
-        info: {
-            get() {
-                switch (language.current) {
-                    case 'pt':
-                        return portfolio_pt_pt;
-                    case 'en':
-                    default:
-                        return portfolio_en;
-                }
-            },
-        }
-    }
+        info() {
+            return getLanguage(language.current).portfolio;
+        },
+    },
 });
 
+// Footer Component
 app.component('vcontact', {
     template: '#vue_contact',
     computed: {
-        info: {
-            get() {
-                switch (language.current) {
-                    case 'pt':
-                        return contact_pt_pt;
-                    case 'en':
-                    default:
-                        return contact_en;
-                }
-            },
-
-        }
+        info() {
+            return getLanguage(language.current).contact;
+        },
     },
     methods: {
         validateForm() {
-            document.querySelectorAll('.needs-validation').forEach(form => form.classList.add('was-validated'));
+            document.querySelectorAll('.needs-validation').forEach((form) => form.classList.add('was-validated'));
         },
         submitForm(event) {
             event.preventDefault();
             this.validateForm(event);
             if (event.target.checkValidity()) {
                 const form = event.target;
-                document.querySelectorAll('.needs-validation').forEach(form => form.classList.remove('was-validated'));
+                document.querySelectorAll('.needs-validation').forEach((form) => form.classList.remove('was-validated'));
                 form.reset();
                 bootstrap.Toast.getOrCreateInstance(document.getElementById('contact-toast')).show();
             }
         },
-    }
+    },
 });
 
-app.mount('#app')
+/***********************
+ Resume Components
+ ***********************/
+
+app.component('vresumebanner', {
+    template: '#vue_resume_banner',
+    computed: {
+        info() {
+            return getLanguage(language.current).timeline_banner;
+        },
+    },
+});
+
+app.component('vtimeline', {
+    template: '#vue_timeline',
+    computed: {
+        info() {
+            return getLanguage(language.current).timeline;
+        },
+        labels() {
+            return getLanguage(language.current).timelineLabels;
+        },
+    },
+    methods: {
+        getExperienceLinks(y) {
+            return this.info
+                .reduce((acc, year) => {
+                    return [...acc, ...year.items];
+                }, [])
+                .filter((item) => {
+                    if (item.type !== 'professional') return false;
+                    const startDate = new Date(item.startDate).getFullYear();
+                    const endDate = new Date(item.endDate).getFullYear();
+                    return startDate <= y && endDate > y;
+                });
+        },
+    },
+});
+
+app.component('vcolapse', {
+    template: '#vue_colapse',
+    props: ['item', 'linkid'],
+    setup() {
+        const bsCollapse = Vue.reactive({
+            collapse: null,
+            toggleCollapse() {
+                this.collapse.toggle();
+            },
+        });
+        return {
+            bsCollapse,
+        };
+    },
+    mounted() {
+        this.bsCollapse.collapse = new bootstrap.Collapse(`#collapse_${this.linkid}`, {toggle: false});
+    },
+    methods: {
+        toogleCollapse() {
+            this.bsCollapse.toggleCollapse();
+        },
+    },
+    computed: {
+        info() {
+            return getLanguage(language.current).timelineLabels;
+        },
+        dateRange() {
+            if (this.item.type === 'certification' && this.item.completionDate) return this.item.completionDate.getFullYear();
+            else if (this.item.startDate && this.item.endDate) return getTimeRange(this.item.startDate, this.item.endDate);
+            return '';
+        },
+    },
+});
+
+app.mount('#app');
 
 // Enable Bootstrap Tooltip
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
